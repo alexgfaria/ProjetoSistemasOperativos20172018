accba429b2358bed9a79765b53f5c031c59f61c9
Alex Faria <alex.faria@me.com>
2017-10-25 23:11:15 +0100

    Correções

--- ProjSO/ProjSO/monitor.c
+++ ProjSO/ProjSO/monitor.c
@@ -8,5 +8,38 @@
 
 #include "Header.h"
 
+/*//apontador para FILE
+FILE *fp;
+*/
 
-//FILE * Resultado;
+
+void escrita(){
+    FILE* logs;
+    
+    logs = fopen("Logs.txt", "a");
+    
+    if(logs == NULL){
+        printf("ERRO AO CRIAR FICHEIRO");
+    }
+    else{
+        fprintf(logs, "---SIMULAÇÃO INICIADA---\n");
+        fprintf(logs, "TEMPO_ESPERA:30\n");
+        fprintf(logs, "TEMPO_DISCO:40\n");
+        fprintf(logs, "CAPACIDADE_FILA:20\n");
+        fprintf(logs, "CAPACIDADE_SALA:10\n");
+        fprintf(logs, "PROB_DESISTE_FILA:100\n");
+        fprintf(logs, "---SIMULAÇÃO TERMINADA---\n");
+    }
+    
+    fclose(logs);
+}
+
+
+int main()
+{
+    escrita();
+    printf("Ficheiro Logs.txt criado com exito!\n");
+    
+    return 0;
+    
+}
--- ProjSO/ProjSO/simulador.c
+++ ProjSO/ProjSO/simulador.c
@@ -25,7 +25,7 @@ CAPACIDADE_CARROS,
 NUMERO_CARROS;
 
 
-int * lerConfig(char * ficheiro){
+/*int * lerConfig(char * ficheiro){
     
     int * a = (int *) malloc (sizeof (int) * 10);
     FILE * fp = fopen (ficheiro, "r"); //r - read
@@ -40,6 +40,12 @@ int * lerConfig(char * ficheiro){
             a[i++]=num;
     }
     fclose(fp);
+    
+    //testa se o ficheiro foi fechado sem erro
+    if(fclose(fp) == EOF) {
+        printf("Error closing file\n");
+    }
+    
     return a;
 }
 
@@ -61,3 +67,77 @@ int main(int argc, char *argv[]){
 
     return 0;
 }
+
+*/
+
+
+
+
+
+int numCliente;
+int tempoGuiche, tempoViagem, tempoSimulacao, capacidadeFila, capacidadeCarro, probDesisteGuiche, probDesisteFila;
+
+//Leitura do ficheiro configSim
+void readFile(){
+    FILE* configFile;
+    
+    configFile = fopen("/Users/alex/Documents/GitHub/ProjetoSistemasOperativos20172018/ProjSO/ProjSO/ConfigSim.txt", "r"); //r = read
+    
+    if(configFile != NULL) {
+        char linha[50];
+        char * valor;
+        while(fgets(linha, sizeof(linha), configFile) != NULL) {
+            valor = strtok(linha, ":");
+            if(strcmp(valor, "TEMPO_GUICHE") == 0) {
+                valor = strtok(NULL, ":");
+                tempoGuiche = atoi(valor);
+            }
+            if(strcmp(valor, "TEMPO_VIAGEM") == 0) {
+                valor = strtok(NULL, ":");
+                tempoViagem = atoi(valor);
+            }
+            if(strcmp(valor, "TEMPO_SIMULACAO") == 0) {
+                valor = strtok(NULL, ":");
+                tempoSimulacao = atoi(valor);
+            }
+            if(strcmp(valor, "CAPACIDADE_FILA") == 0) {
+                valor = strtok(NULL, ":");
+                capacidadeFila = atoi(valor);
+            }
+            if(strcmp(valor, "CAPACIDADE_CARRO") == 0) {
+                valor = strtok(NULL, ":");
+                capacidadeCarro = atoi(valor);
+            }
+            if(strcmp(valor, "PROB_DESISTE_GUICHE") == 0) {
+                valor = strtok(NULL, ":");
+                probDesisteGuiche = atoi(valor);
+            }
+            if(strcmp(valor, "PROB_DESISTE_FILA") == 0) {
+                valor = strtok(NULL, ":");
+                probDesisteFila = atoi(valor);
+            }
+            
+        }
+    }
+    else {
+        printf("ERRO AO ABRIR FICHEIRO\n");
+    }
+    
+    fclose(configFile);
+}
+
+
+
+int main(){
+    readFile();
+    printf("O tempo de espera no guiche e': %d\n",tempoGuiche);
+    printf("A viagem tem a duracao: %d\n",tempoViagem);
+    printf("O tempo da simulacao': %d\n",tempoSimulacao);
+    printf("A capacidade da fila e' %d\n",capacidadeFila);
+    printf("A capacidade de cada carro e': %d\n",capacidadeCarro);
+    printf("A probablidade de desistencia da fila do Guiche' e': %d\n",probDesisteGuiche);
+    printf("A probablidade de desistencia da fila e': %d\n",probDesisteFila);
+    return 0;
+    
+}
+
