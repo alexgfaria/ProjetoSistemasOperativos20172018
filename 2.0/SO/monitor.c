0b86e65382e07a033cdd8f4b9cc7be5d2ebea294
Alex Faria <alex.faria@me.com>
2017-12-06 14:49:50 +0000

    commit 1

---
+++ 2.0/SO/monitor.c
@@ -0,0 +1,172 @@
+#include <pthread.h>
+#include <string.h>
+#include "unix.h"
+#include "util.h"
+
+//apontador para o ficheiro onde será escrito o relatório da simulação
+FILE * relatorio;
+
+//variáveis globais para a estatística
+int num_fila_normal=0, num_carros=2, num_cli_normal=0,  desiste_espera=0, desiste_medo=0, num_viagens=0, inicio_simulacao=0, fim_simulacao=0, soma_espera_normal=0, esperou_normal=0;
+
+//variáveis globais para controlo de execução
+int corre=0, pausa=0;
+
+
+void mostra_estatistica()
+{
+	if(corre && !pausa)
+		printf(" 1. Estado atual: Simulacao a decorrer.\n");
+	else
+	{
+		if(pausa)
+			printf(" 1. Estado atual: Simulação em pausa.\n");
+		else
+			printf(" 1. Estado atual: Simulação terminada.\n");
+	}
+	printf(" 2. Tamanho atual da fila de espera para Clientes Normais: %d\n", num_fila_normal);
+	printf(" 4. Número atual de Carros: %d\n", num_carros);
+	printf(" 5. Clientes Normais: %d\n", num_fila_normal);
+	printf(" 8. Desistências na Fila de espera: %d\n", desiste_espera);
+	printf(" 9. Desistências Por medo: %d\n", desiste_medo);
+	printf("10. Total de desistências: %d\n", desiste_espera + desiste_medo);
+	printf("11. Número de viagens: %d\n", num_viagens);
+}
+
+
+//função que fica à escuta das mensagens do simulador
+
+void *escuta_comunicacao(void *arg)
+{
+	int sockfd=*((int *) arg);
+	int msg, tempo=0, num_comandos, id, com;
+	char buffer[256],comando[20];
+	
+	//ciclo que fica à espera de mensagens do simulador
+	
+	while(1)
+	{
+		if((msg=recv(sockfd, buffer, sizeof(buffer), 0)) <= 0)
+		{
+			if(msg < 0)
+				perror("recv");
+		}
+		else
+		{
+           	buffer[msg]='\0';
+			fprintf(relatorio,"%s", buffer);
+
+			num_comandos = sscanf(buffer,"%d %s %d %d", &tempo, comando, &id, &com);
+
+			if(num_comandos>0) 
+			{
+				if(!strcmp(comando,"INICIO"))
+				{
+					inicio_simulacao = tempo;
+				}
+				else if(!strcmp(comando,"CHEGADA"))
+				{
+						num_fila_normal++;
+				}
+				else if(!strcmp(comando,"DESISTE_FILA"))
+				{
+					desiste_espera++;
+				}
+				else if(!strcmp(comando,"ENTRA_EMBARQUE"))
+				{
+						soma_espera_normal += tempo;
+						esperou_normal++;
+					
+				}
+				else if(!strcmp(comando,"DESISTE_EMBARQUE"))
+				{
+					desiste_medo++;
+				}
+			}
+		}
+	}
+	return NULL;
+}
+
+
+//função principal do monitor
+
+int main(int argc, char *argv[])
+{	
+	struct sockaddr_un serv_addr, cli_addr;
+	int sockfd, servlen, newsockfd;
+
+	int clilen=sizeof(cli_addr);
+	char buffer[256];
+	char menu[512] = "----------------------------------\n| comandos disponiveis no monitor|\n----------------------------------\n| inicio - Comecar simulacao     |\n| pausa  - Pausar simulacao      |\n| retomar - Retomar simulacao    |\n| estado - Estado da simulacao   |\n| sair   - Sair do monitor       |\n----------------------------------\n";
+
+	//Criacao do socket UNIX
+	if((sockfd=socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
+		perror("cant open socket stream");
+	serv_addr.sun_family=AF_UNIX;
+	strcpy(serv_addr.sun_path, UNIXSTR_PATH);
+	servlen=strlen(serv_addr.sun_path)+sizeof(serv_addr.sun_family);
+	unlink(UNIXSTR_PATH);
+	if(bind(sockfd, (struct sockaddr *) &serv_addr, servlen) < 0)
+		perror("cant bind local address");
+	listen(sockfd, 1);
+
+	if((newsockfd=accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) < 0)
+		perror("accept error");
+
+	//criacao da tarefa que ira tratar da comunicação com o simulador
+	
+	pthread_t thread;
+	pthread_create(&thread, NULL, &escuta_comunicacao, &newsockfd);
+
+	//Ciclo que espera pela informacao vinda da consola
+	
+	printf("%s",menu);
+
+	unlink("relatorio.log");	
+	relatorio=fopen("relatorio.log", "a");
+
+	do
+	{
+		fgets(buffer, sizeof(buffer), stdin);
+
+
+
+		if(send(newsockfd, buffer, sizeof(buffer), 0) == -1)
+		{
+			perror("send");
+			exit(1);
+		}
+
+		if(!strcmp (buffer, "inicio\n"))
+		{
+			corre=1;
+			pausa=0;
+		}
+		if(!strcmp (buffer, "pausa\n"))
+		{
+			pausa=1;
+			printf("Simulacao em pausa - \"retomar\" para continuar\n");
+		}
+		if(!strcmp (buffer, "retomar\n"))
+		{
+			pausa=0;
+		}
+		if(!strcmp (buffer, "estado\n"))
+		{
+			mostra_estatistica();
+		}
+	}
+	while(strcmp (buffer, "sair\n"));
+	
+	corre = 0;
+	pausa = 0;
+
+	printf("\nExecução terminou.\n\n");
+	mostra_estatistica();
+	fclose(relatorio);
+	close(sockfd);
+
+	return 0;
+}
+
---
+++ 2.0/SO/simulador.c
@@ -0,0 +1,342 @@
+
+#include <time.h>
+#include <stdlib.h>
+
+#include <semaphore.h>
+#include <string.h>
+#include <pthread.h>
+#include <signal.h>
+#include "unix.h"
+#include "util.h"
+
+
+//variaveis globais
+
+
+
+
+
+int num_cliente, num_carro, num_emb, carro, carros_em_servico;
+	
+time_t start;
+	
+struct sockaddr_un serv_addr;
+int sockfd, servlen;
+
+//variaveis com informacao da configuracao
+int INICIO_FINAL, TEMPO_SIMULACAO, TEMPO_MEDIO_CHEGADA_CLIENTES, PROB_DESISTE_ESPERA,  PROB_DESISTE_MEDO,  TEMPO_VIAGEM, CAPACIDADE_CARROS, NUMERO_CARROS;
+
+int corre=0,pausa=0,controlo_pausa;
+
+
+	//funcao: tarefa_cliente
+
+void *tarefa_cliente(void *ptr)
+{
+	pthread_detach(pthread_self());
+	int car, g;
+	time_t temp_i;
+	char buffer_c[256];
+	int tem_esp = rand()%10; // entre 0 e 10 minutos de espera
+	
+	
+	
+	int num_cli = num_cliente++;
+	printf("O cliente %d chegou a fila de espera.\n", num_cli);
+	sprintf(buffer_c, "%d CHEGADA %d 0\n", (int)time(0), num_cli);
+	send(sockfd,buffer_c,sizeof(buffer_c),0);
+	
+	temp_i = time(0);
+	
+		
+	//desistencia por tempo
+	
+	if((int)(time(0) - temp_i) > tem_esp)
+	{
+		g = rand()%100;
+		if(g <= PROB_DESISTE_ESPERA)
+		{
+			if(tem_esp == 1)
+				printf("O cliente %d desistiu ao fim de %d minuto a espera.\n", num_cli, tem_esp);
+			else
+				printf("O cliente %d desistiu ao fim de %d minutos a espera.\n", num_cli, tem_esp);
+			sprintf(buffer_c, "%d DESISTE_FILA %d 0\n", (int)time(0), num_cli);
+			send(sockfd,buffer_c,sizeof(buffer_c),0);
+			return NULL;
+		}
+	}
+
+	
+	
+	//desistencia por medo
+	
+	g = rand()%100;
+	if(g <= PROB_DESISTE_MEDO)
+	{
+		printf("O cliente %d entrou na zona de embarque.\n",num_cli);
+		printf("O cliente %d desistiu porque ficou com medo.\n", num_cli);
+		sprintf(buffer_c, "%d DESISTE_EMBARQUE\n", (int)time(0));
+		send(sockfd,buffer_c,sizeof(buffer_c),0);
+		return NULL;
+	}
+	
+	num_emb++;
+	
+	if ( num_emb < CAPACIDADE_CARROS)
+		
+
+	printf("O cliente %d entrou na zona de embarque.\n",num_cli);
+	sprintf(buffer_c, "%d ENTRA_EMBARQUE %d 0\n", (int)(time(0)-temp_i), num_cli);
+	send(sockfd,buffer_c,sizeof(buffer_c),0);
+	
+	car = carro;
+	printf("O cliente %d entrou no carro %d.\n", num_cli, car);
+	sprintf(buffer_c, "%d EMBARQUE %d %d\n", (int)time(0), num_cli, car);
+	send(sockfd,buffer_c,sizeof(buffer_c),0);
+	
+	
+	printf("O cliente %d saiu do carro %d.\n", num_cli, car);
+	sprintf(buffer_c, "%d DESEMBARQUE %d %d\n", (int)time(0), num_cli, car);
+	send(sockfd,buffer_c,sizeof(buffer_c),0);
+	
+	
+	return NULL;
+}
+
+
+//funcao: tarefa_carro
+
+void *tarefa_carro(void *ptr)
+{
+	
+	pthread_detach(pthread_self());
+	int id = num_carro++;
+	char buffer_c[256];
+	time_t hora_partida, hora_i;
+	char variavel[20] = "carro";
+	
+
+	
+	void inicia_viagem()
+	{
+		int i;
+		for(i = 0; i < CAPACIDADE_CARROS; i++)
+			
+			
+		printf("O carro %d iniciou viagem.\n", id);
+		sprintf(buffer_c, "%d ARRANQUE %d\n", (int)time(0), id);
+		send(sockfd,buffer_c,sizeof(buffer_c),0);
+		
+		
+		hora_partida = time(0);
+		
+		while((int)(time(0) - hora_partida) < TEMPO_VIAGEM); 
+		printf("O carro %d terminou a viagem e chegou a zona de desembarque.\n", id);
+		sprintf(buffer_c, "%d FINAL_VIAGEM %d\n", (int)time(0), id);
+		send(sockfd,buffer_c,sizeof(buffer_c),0);
+	}
+	
+	
+	
+	while(1)
+	{
+		
+		printf("O carro %d ficou em lista de espera.\n", id);
+		sprintf(buffer_c, "%d ESPERA %d\n", (int)time(0), id);
+		send(sockfd,buffer_c,sizeof(buffer_c),0);
+		printf("O carro %d chegou a zona de embarque.\n", id);
+		sprintf(buffer_c, "%d DISPONIVEL %d\n", (int)time(0), id);
+		send(sockfd,buffer_c,sizeof(buffer_c),0);
+		int i;
+		num_emb = 0;
+		
+		if(i == 0)
+		for(i = 0; i < CAPACIDADE_CARROS; i++)
+		carro = id;	
+		hora_i = time(0);
+		
+		if(time(0) > start + TEMPO_SIMULACAO)
+		{	
+			usleep(100000);	
+			if(num_emb == 0)
+			{
+				printf("O parque ja fechou e nao tem clientes em espera.\n");
+				printf("O carro %d vai ser arrumado.\n", id);
+				carros_em_servico--;
+				return NULL;
+			}
+			
+			for(i = 0; i < CAPACIDADE_CARROS; i++)
+			printf("O carro %d iniciou viagem.\n", id);
+			sprintf(buffer_c, "%d ARRANQUE %d\n", (int)time(0), id);
+			send(sockfd,buffer_c,sizeof(buffer_c),0);
+			
+			
+			hora_partida = time(0);
+			while((int)(time(0) - hora_partida) < TEMPO_VIAGEM); 
+			printf("O carro %d terminou a viagem e chegou a zona de desembarque.\n", id);
+			sprintf(buffer_c, "%d FINAL_VIAGEM %d\n", (int)time(0), id);
+			send(sockfd,buffer_c,sizeof(buffer_c),0);
+		}
+		else
+		{	
+			while(num_emb < CAPACIDADE_CARROS && time(0) < start + TEMPO_SIMULACAO);
+			if(num_emb == CAPACIDADE_CARROS)
+			{
+					usleep(100000);
+					inicia_viagem();
+			}
+			else
+			{
+					usleep(100000);	
+
+					if(num_emb == 0)
+					{
+						printf("O parque ja fechou e nao tem clientes em espera.\n");
+						printf("O carro %d vai ser arrumado.\n", id);
+						carros_em_servico--;
+						return NULL;
+					}			
+			
+					inicia_viagem();
+			}
+		}
+	}
+	return NULL;
+}
+
+//Funcao que trata dos pedidos vindos do Monitor 
+void *recebe_comandos_monitor(void *arg)
+{
+	struct sockaddr_un cli_addr;
+	int done, n, id;
+
+	int sockfd=*((int *) arg), clilen=sizeof(cli_addr);
+
+	char buffer[256];
+	
+	//Ciclo que fica a espera dos pedidos dos Monitor, para lhe dar resposta adequada
+	while(1)
+	{
+		done=0;	
+		if((n=recv(sockfd, buffer, sizeof(buffer), 0)) <= 0)
+		{
+			if(n < 0) 
+				perror("recv error");
+			done=1;
+		}
+		buffer[n]='\0';
+
+		if(!strcmp(buffer, "termina\n"))
+		{
+			corre=0;
+			exit(1);
+		}
+		else
+		{	
+			if(!strcmp(buffer, "inicio\n"))
+				corre = 1;
+			if(!strcmp(buffer, "pausa\n"))
+				pausa = 1;
+			if(!strcmp(buffer, "retomar\n"))
+				pausa = 0;
+		}
+	}
+	return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+
+	srand(time(NULL));
+	
+	num_cliente=1;
+	num_carro=1;
+	carro = NUMERO_CARROS;
+	num_emb = 0;
+	
+	if(argc<2)
+	{
+		printf("Falta o ficheiro de configuração. Não é possível continuar.");
+		return 1;
+	}
+	else
+	{
+	
+		//interpretacao do ficheiro de configuraçao
+		int *conf = leitura_configuracao(argv[1]);
+		INICIO_FINAL					= conf[0];
+		TEMPO_SIMULACAO					= conf[1];
+		TEMPO_MEDIO_CHEGADA_CLIENTES	= conf[2];
+		PROB_DESISTE_ESPERA				= conf[3];
+		TEMPO_VIAGEM					= conf[7];
+		CAPACIDADE_CARROS				= conf[8];
+		NUMERO_CARROS					= conf[9];
+		
+		
+		
+		
+		//criacao do socket e ligação
+		if((sockfd=socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
+			perror("Simulador: cant open socket stream");
+		serv_addr.sun_family=AF_UNIX;
+		strcpy(serv_addr.sun_path, UNIXSTR_PATH);
+		servlen=strlen(serv_addr.sun_path)+sizeof(serv_addr.sun_family);
+		if(connect(sockfd, (struct sockaddr *) &serv_addr, servlen) < 0)
+			perror("connect error");
+
+		//Criacao da tarefa que ira tratar dos pedidos enviados pelo Monitor
+		pthread_t thread;
+		pthread_create(&thread, NULL, &recebe_comandos_monitor, &sockfd);
+		
+		while(!corre);
+
+		int i, r, h;
+		char buffer[256];
+		carros_em_servico = NUMERO_CARROS;
+		
+		
+		start = time(0);
+		sprintf(buffer, "%d INICIO\n", (int)start);
+		send(sockfd,buffer,sizeof(buffer),0);
+		
+		TEMPO_SIMULACAO *= 60;
+		
+		for(i = 0; i < NUMERO_CARROS; i++)
+		{
+			pthread_create(&thread, NULL, &tarefa_carro, &sockfd);
+		}
+			
+		while((int)(time(0) - start) < TEMPO_SIMULACAO)
+		{
+				pthread_create(&thread, NULL, &tarefa_cliente, &sockfd);
+			
+			h = rand()%3;
+			h = h * 100000;
+				h = 0 - h;
+			usleep((TEMPO_MEDIO_CHEGADA_CLIENTES*1000000) + h);	
+			
+			if(pausa)
+			{		
+				controlo_pausa = 1;
+			}
+			while(pausa);
+			if(controlo_pausa)
+			{
+				controlo_pausa = 0;
+			}
+
+		}
+
+		while(carros_em_servico>0);
+		printf("\nO parque já fechou e não existem mais clientes em espera.\n");
+		printf("\nSimulação terminada.\n");
+		sprintf(buffer,"%d FIM \n",(int) time(0));
+		send(sockfd,buffer,sizeof(buffer),0);
+
+		while(corre);
+		
+		close(sockfd);
+	
+	}return 0;
+}
